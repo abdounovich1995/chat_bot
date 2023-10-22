e433c9867e312ed297ca4993d8bbfcb4e0cf8385
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 12:32:21 +0100

    aaa

--- getStartedPayload.js
+++ getStartedPayload.js
@@ -1,44 +1,41 @@
 const request = require('request');
 
-function sendTextMessage(senderPsid, text) {
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+function sendWelcomeMessage(senderPsid, username) {
+  const welcomeMessage = `Hello, ${username}! Welcome to the Messenger bot.`;
+
+  sendResponse(senderPsid, welcomeMessage);
+}
+
+function sendResponse(senderPsid, response) {
   const requestBody = {
     recipient: {
-      id: senderPsid
+      id: senderPsid,
     },
     message: {
-      text
-    }
+      text: response,
+    },
   };
 
-  callSendAPI(requestBody);
-}
-
-function handlePostback(senderPsid, postback) {
-  const payload = postback.payload;
-
-  if (payload === 'GET_STARTED_PAYLOAD') {
-    sendTextMessage(senderPsid, 'Welcome! How can I assist you?');
-  }
-}
-
-function callSendAPI(requestBody) {
-  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-  request({
-    uri: 'https://graph.facebook.com/v13.0/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: requestBody
-  }, (err, _res, _body) => {
-    if (!err) {
-      console.log('Message sent');
-    } else {
-      console.error('Unable to send message:', err);
+  request(
+    {
+      uri: 'https://graph.facebook.com/v13.0/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: requestBody,
+    },
+    (err, _res, _body) => {
+      if (!err) {
+        console.log('Message sent');
+      } else {
+        console.error('Unable to send message:', err);
+      }
     }
-  });
+  );
 }
 
 module.exports = {
-  sendTextMessage,
-  handlePostback
+  sendWelcomeMessage,
+  sendResponse,
 };
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const getStartedPayload = require('./getStartedPayload'); // Import the renamed module
 const request = require('request');
+const messengerBot = require('./messengerBot'); // Import the messengerBot module
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your own verify token
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your Page Access Token
 
 app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
@@ -29,17 +30,23 @@ app.post('/webhook', (req, res) => {
     body.entry.forEach((entry) => {
       const webhookEvent = entry.messaging[0];
 
-      if (webhookEvent.message) {
+      if (webhookEvent.postback) {
+        // Handle the "Get Started" button click event
+        if (webhookEvent.postback.payload === 'GET_STARTED_PAYLOAD') {
+          const senderPsid = webhookEvent.sender.id;
+          const username = "User's Name"; // Replace with the actual username if available
+          messengerBot.sendWelcomeMessage(senderPsid, username);
+        }
+      } else if (webhookEvent.message) {
+        // Handle incoming messages
         const senderPsid = webhookEvent.sender.id;
         const messageText = webhookEvent.message.text;
 
         if (messageText.toLowerCase() === 'hello') {
-          getStartedPayload.sendTextMessage(senderPsid, 'hi');
+          messengerBot.sendResponse(senderPsid, 'hi');
         } else {
-          getStartedPayload.sendTextMessage(senderPsid, 'I don\'t understand');
+          messengerBot.sendResponse(senderPsid, "I don't understand");
         }
-      } else if (webhookEvent.postback) {
-        getStartedPayload.handlePostback(senderPsid, webhookEvent.postback);
       }
     });
 
