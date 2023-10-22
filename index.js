02a9adfd75cc4d7dd47dfdf631aeca953001db16
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 12:39:03 +0100

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const messengerBot = require('./messengerBot'); // Import the messengerBot module
+const messengerBot = require('./payloads'); // Import the messengerBot module
 
 const app = express();
 app.use(bodyParser.json());
