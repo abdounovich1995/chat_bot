4b4e883250777e1f159050f95d841c1e3f5f5cff
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 12:14:22 +0100

    add

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const request = require('request');
 
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your own verify token
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your Page Access Token
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  const body = req.body;
+
+  if (body.object === 'page') {
+    body.entry.forEach((entry) => {
+      const webhookEvent = entry.messaging[0];
+
+      if (webhookEvent.message) {
+        const senderPsid = webhookEvent.sender.id;
+        const messageText = webhookEvent.message.text;
+
+        if (messageText.toLowerCase() === 'hello') {
+          sendResponse(senderPsid, 'hi');
+        }
+      }
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+function sendResponse(senderPsid, response) {
+  const requestBody = {
+    recipient: {
+      id: senderPsid
+    },
+    message: {
+      text: response
+    }
+  };
+
+  request({
+    uri: 'https://graph.facebook.com/v13.0/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: requestBody
+  }, (err, _res, _body) => {
+    if (!err) {
+      console.log('Message sent');
+    } else {
+      console.error('Unable to send message:', err);
+    }
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your own token
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-  
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  });
-  
-  app.post('/webhook', (req, res) => {
-    // Handle incoming messages here
-  });
-  
