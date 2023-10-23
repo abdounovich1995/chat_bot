35c63640c04e124f3a70ddb1af1288ccc92de306
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-23 10:27:02 +0100

    h

--- payloads.js
+++ payloads.js
@@ -1,36 +1,44 @@
 const request = require('request');
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-
-
-function sendResponse(senderPsid, response) {
+function sendTextMessage(senderPsid, text) {
   const requestBody = {
     recipient: {
-      id: senderPsid,
+      id: senderPsid
     },
     message: {
-      text: response,
-    },
+      text
+    }
   };
 
-  request(
-    {
-      uri: 'https://graph.facebook.com/v13.0/me/messages',
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: 'POST',
-      json: requestBody,
-    },
-    (err, _res, _body) => {
-      if (!err) {
-        console.log('Message sent');
-      } else {
-        console.error('Unable to send message:', err);
-      }
+  callSendAPI(requestBody);
+}
+
+function handlePostback(senderPsid, postback) {
+  const payload = postback.payload;
+
+  if (payload === 'GET_STARTED_PAYLOAD') {
+    sendTextMessage(senderPsid, 'Welcome! How can I assist you?');
+  }
+}
+
+function callSendAPI(requestBody) {
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+  request({
+    uri: 'https://graph.facebook.com/v13.0/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: requestBody
+  }, (err, _res, _body) => {
+    if (!err) {
+      console.log('Message sent');
+    } else {
+      console.error('Unable to send message:', err);
     }
-  );
+  });
 }
 
 module.exports = {
-  sendResponse,
+  sendTextMessage,
+  handlePostback
 };
