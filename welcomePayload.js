10cd943cc7f2da10ea396bc924c24022556c6b78
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-23 10:19:38 +0100

    aaa

--- welcomePayload.js
+++ welcomePayload.js
@@ -2,12 +2,16 @@
 const messageManager = require('./messageManager');
 
 // Handle the "Get Started" payload
-function handleGetStarted(senderPsid) {
-  // You can customize the response message here
-  const response = "Welcome to the bot! You can start by typing a message or use the menu.";
+async function handleGetStarted(senderPsid) {
+  try {
+    // You can customize the response message here
+    const response = "Welcome to the bot! You can start by typing a message or use the menu.";
 
-  // Send the response to the user
-  messageManager.sendTextMessage(senderPsid, response);
+    // Send the response to the user
+    await messageManager.sendTextMessage(senderPsid, response);
+  } catch (error) {
+    console.error('Error handling Get Started:', error);
+  }
 }
 
 // Export the function to be used in other files
