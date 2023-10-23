7091d1116418a9d3988b8b57576c4cff33f51f54
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-23 10:32:08 +0100

    g

--- index.js
+++ index.js
@@ -1,89 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const senderAction = require('./senderAction');
-const persistentMenu = require('./persistentMenu');
-const messageManager = require('./messageManager');
-const payloads = require('./payloads');
-const verifyWebhook = require('./webhookVerification');
-const firebaseService = require('./firebaseService');
-const getStartedHandler = require('./welcomePayload'); // Import the getStartedHandler module
+
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-// Set the persistent menu using the imported configuration
-function setPersistentMenu() {
-  axios.post('https://graph.facebook.com/v13.0/me/messenger_profile', {
-    persistent_menu: persistentMenu,
-  }, {
-    params: { access_token: PAGE_ACCESS_TOKEN },
-  })
-    .then(() => {
-      console.log('Persistent menu set successfully');
-    })
-    .catch((error) => {
-      console.error('Unable to set persistent menu:', error);
-    });
-}
-
-// Create a route to set the menu when /setMenu is accessed in the browser
-app.get('/setMenu', (req, res) => {
-  // Set the persistent menu
-  setPersistentMenu();
-  res.send('Persistent menu set successfully');
-});
-
-// Handle Facebook Webhook verification using the imported function
-app.get('/webhook', verifyWebhook);
-
-// Handle Facebook Webhook events
-app.post('/webhook', async (req, res) => {
-  const body = req.body;
-
-  if (body.object === 'page') {
-    body.entry.forEach(async (entry) => {
-      const webhookEvent = entry.messaging[0];
-
-      if (webhookEvent.postback) {
-        if (webhookEvent.postback.payload === payloads.GET_STARTED_PAYLOAD) {
-          const senderPsid = webhookEvent.sender.id;
-          console.log("hello");
-          getStartedHandler.handleGetStarted(senderPsid); // Use the new handler for "Get Started" payload
-          console.log("welcome");
-        }
-      } else if (webhookEvent.message) {
-        const senderPsid = webhookEvent.sender.id;
-        const messageText = webhookEvent.message.text;
-
-        if (messageText.toLowerCase() === 'aaa') {
-          messageManager.sendQuickReply(senderPsid, 'Choose an option:');
-        } else if (messageText.toLowerCase() === 'hello') {
-          messageManager.sendTextMessage(senderPsid, 'Hi');
-        } else if (messageText.toLowerCase() === 'b') {
-          firebaseService.addUserToClientCollection(senderPsid)
-            .then((docRef) => {
-              console.log('User information added to Firebase: ', docRef.id);
-            })
-            .catch((error) => {
-              console.error('Error adding user information to Firebase: ', error);
-            });
-          // Respond to the user
-          messageManager.sendTextMessage(senderPsid, 'User information added to Firebase "client" collection.');
-        } else {
-          messageManager.sendTextMessage(senderPsid, "I don't understand");
-        }
-      }
-    });
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    res.sendStatus(404);
-  }
-});
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your own verify token
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your Page Access Token
 
 // Function to get the user's name
 async function getUserName(senderPsid) {
@@ -103,7 +30,49 @@ async function getUserName(senderPsid) {
   }
 }
 
-// Start the Express server
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', async (req, res) => {
+  const body = req.body;
+
+  if (body.object === 'page') {
+    body.entry.forEach(async (entry) => {
+      const webhookEvent = entry.messaging[0];
+
+      if (webhookEvent.postback) {
+        if (webhookEvent.postback.payload === 'GET_STARTED_PAYLOAD') {
+          const senderPsid = webhookEvent.sender.id;
+          const username = await getUserName(senderPsid); // Get the user's name
+          messengerBot.sendWelcomeMessage(senderPsid, username);
+        }
+      } else if (webhookEvent.message) {
+        const senderPsid = webhookEvent.sender.id;
+        const messageText = webhookEvent.message.text;
+
+        if (messageText.toLowerCase() === 'hello') {
+          messengerBot.sendResponse(senderPsid, 'hi');
+        } else {
+          messengerBot.sendResponse(senderPsid, "I don't understand");
+        }
+      }
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
