2d17bc4997508a08f475583103024d15d765d7c5
abdounovich <merahi.abdeldjalil95@gmail.com>
2023-10-25 20:45:50 +0200

    i hope

--- templates/welcomeButton.js
+++ templates/welcomeButton.js
@@ -1,4 +1,4 @@
-
+const admin = require('firebase-admin');
 const axios = require('axios'); // Import the axios library
 const firebaseService = require('../firebaseService'); // Import your Firebase service module here
 
@@ -7,7 +7,41 @@ const SITE_URL = process.env.SITE_URL;
 
 
 
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://chatbotapp-3e7c8.firebaseio.com',
+});
+
+const db = admin.firestore();
+
+
+const clientsCollection = db.collection('clients');
+
+
+
+async function getClientReferenceByPSID(userPSID) {
+  try {
+    const querySnapshot = await clientsCollection.where('userID', '==', userPSID).get();
+
+    if (!querySnapshot.empty) {
+      // User with the given PSID exists, return the reference to the client document
+      const clientDocument = querySnapshot.docs[0];
+      const clientReference = clientDocument.id;
+      return clientReference;
+    } else {
+      // User with the given PSID does not exist
+      return null;
+    }
+  } catch (error) {
+    console.error('Error retrieving client reference:', error);
+    throw error; // You can choose to handle the error differently
+  }
+}
+
   async function sendButtonTemplate(userId) {
+    const clientRef = await getClientReferenceByPSID(userId);
 
 try {
 
@@ -25,7 +59,7 @@ try {
             buttons: [
               {
                 type: 'web_url', // Change the button type to 'web_url'
-                url: `${SITE_URL}/clientAddAppointement?clientPSID `,
+                url: `${SITE_URL}/clientAddAppointement?clientPSID=${clientRef} `,
                 title: 'now',
               },
 
@@ -56,5 +90,5 @@ try {
 
   module.exports = {
     sendButtonTemplate,
-  
+    getClientReferenceByPSID
   };
