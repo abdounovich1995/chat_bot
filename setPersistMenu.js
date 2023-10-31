7330d11c62d537998e0f932c3a4df360309eb59a
abdounovich <merahi.abdeldjalil95@gmail.com>
2023-10-31 20:48:07 +0100

    try

--- setPersistMenu.js
+++ setPersistMenu.js
@@ -1,45 +1,46 @@
 const axios = require('axios'); // Import the axios package for making HTTP requests
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your actual Page Access Token
-
 function setPersistentMenu(psid) {
-    const userMenu = [
-      // Define your user-specific menu here
-      // This should be an array of menu items for the specific user
-      {
-        locale: 'default',
-        composer_input_disabled: false,
-        call_to_actions: [
-          {
-            type: 'postback',
-            title: 'Talk to an agent',
-            payload: 'CARE_HELP',
-          },
-          {
-            type: 'postback',
-            title: 'Outfit suggestions',
-            payload: 'CURATION',
-          },
-          {
-            type: 'web_url',
-            title: 'Shop now',
-            url: 'https://www.originalcoastclothing.com/',
-            webview_height_ratio: 'full',
-          },
-        ],
-      },
-    ];
+    const userPersistentMenu = {
+      psid: psid,
+      persistent_menu: [
+        {
+          locale: 'default',
+          composer_input_disabled: false,
+          call_to_actions: [
+            {
+              type: 'postback',
+              title: 'Talk to an agent',
+              payload: 'CARE_HELP',
+            },
+            {
+              type: 'postback',
+              title: 'Outfit suggestions',
+              payload: 'CURATION',
+            },
+            {
+              type: 'web_url',
+              title: 'Shop now',
+              url: 'https://www.originalcoastclothing.com/',
+              webview_height_ratio: 'full',
+            },
+          ],
+        },
+      ],
+    };
   
-    axios.post(`https://graph.facebook.com/v13.0/${psid}/custom_user_settings?access_token=${PAGE_ACCESS_TOKEN}`, {
-      persistent_menu: userMenu,
-    })
+    const accessToken = PAGE_ACCESS_TOKEN; // Replace with your actual access token
+  
+    axios.post(`https://graph.facebook.com/v18.0/me/custom_user_settings?access_token=${accessToken}`, userPersistentMenu)
       .then(() => {
-        console.log('User-level persistent menu set successfully');
+        console.log('User-level persistent menu set successfully for PSID:', psid);
       })
       .catch((error) => {
         console.error('Unable to set user-level persistent menu:', error);
       });
   }
+  
 
   module.exports = { setPersistentMenu };
 
