19663d04122b055198e0ed0e38ffce739384c535
abdounovich <merahi.abdeldjalil95@gmail.com>
2023-11-02 11:46:59 +0100

    throw

--- firebaseService.js
+++ firebaseService.js
@@ -79,6 +79,7 @@ async function addUserToClientCollection(userId) {
     const clientCollection = db.collection('clients');
 
     clientCollection.add(userInformation);
+    console.log("added................................");
 
     return   welcomeButton.sendButtonTemplate(userId,await getClientReferenceByPSID(userId));
  
