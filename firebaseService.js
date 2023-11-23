7b982f63a8b7bb0eb163f20e9d3b4621bb093c9f
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-23 16:51:05 +0100

    16h53

--- firebaseService.js
+++ firebaseService.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
-const axios = require('axios');
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const messageManager = require('./messageManager');
-const welcomeButton = require('./templates/welcomeButton');
+const axios = require('axios'); // Import the axios library
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your actual Page Access Token
+const messageManager = require('./messageManager'); // Import the messageManager module
+const welcomeButton = require('./templates/welcomeButton'); // Import the messageManager module
 const cron = require('node-cron');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,25 +15,33 @@ admin.initializeApp({
 const db = admin.firestore();
 
 const typesCollection = db.collection('types');
+
 const clientsCollection = db.collection('clients');
+
 
 async function getClientReferenceByPSID(userPSID) {
   try {
     const querySnapshot = await clientsCollection.where('userID', '==', userPSID).get();
 
     if (querySnapshot.empty) {
+      // If there are no matching documents, throw an exception
       throw new Error("No client document found with the specified PSID.");
     }
 
+    // User with the given PSID exists, return the reference to the client document
     const clientDocument = querySnapshot.docs[0];
     const clientReference = clientDocument.id;
     return clientReference;
   } catch (error) {
+    // Handle the exception
     console.error("An error occurred:", error.message);
+    // You can rethrow the exception if needed
     throw error;
   }
 }
 
+
+// Fetch data from the "types" collection
 async function getTypesData() {
   const snapshot = await typesCollection.get();
   const typesData = [];
@@ -83,33 +92,43 @@ async function addUserToClientCollection(userId) {
     }
   } catch (error) {
     console.error('An error occurred:', error.message);
+    // You can handle the error as needed
   }
 }
 
+
 const algeriaTimeZone = 'Africa/Algiers';
 
-// Schedule a cron job to run every day at 00:00
-cron.schedule('49 16 * * *', async () => {
+// Schedule a cron job to run every day at 16:00 in Algeria time zone
+cron.schedule('53 16 * * *', async () => {
   try {
     // Call a function to update "type" field in appointments collection to 0 for today's appointments
     await updateAppointmentsType();
+
     console.log('Cron job executed successfully');
   } catch (error) {
     console.error('Error executing cron job:', error.message);
   }
+}, {
+  timezone: algeriaTimeZone, // Set the time zone
 });
 
 async function updateAppointmentsType() {
   try {
-    // Get the current date
-    const currentDate = new Date();
-    currentDate.setHours(16, 49, 0, 0);
+    // Get the current date in Algeria time zone
+    const currentDate = new Date().toLocaleString('en-US', { timeZone: algeriaTimeZone });
+    
+    // Convert currentDate to a JavaScript Date object
+    const algeriaDate = new Date(currentDate);
+
+    // Set hours and minutes to 16:00
+    algeriaDate.setHours(16, 53, 0, 0);
 
     // Reference to the appointments collection
     const appointmentsCollection = db.collection('appointments');
 
     // Query appointments for today
-    const querySnapshot = await appointmentsCollection.where('date', '==', currentDate).get();
+    const querySnapshot = await appointmentsCollection.where('date', '==', algeriaDate).get();
 
     // Update "type" field to 0 for each document
     const updatePromises = querySnapshot.docs.map(async (doc) => {
@@ -125,6 +144,10 @@ async function updateAppointmentsType() {
     throw error;
   }
 }
+
+
+
+
 
 async function getUserName(userId) {
   try {
@@ -151,13 +174,16 @@ async function getUserInfo(psid) {
         access_token: PAGE_ACCESS_TOKEN,
       },
     });
+  
 
     const userData = response.data;
     const firstName = userData.first_name;
     const lastName = userData.last_name;
     const profilePicture = userData.profile_pic;
 
-    return { firstName, lastName, profilePicture };
+
+
+    return { firstName, lastName ,profilePicture};
   } catch (error) {
     console.error(`Error fetching user info: ${error.message}`);
     return null;
