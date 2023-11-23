1641c6678c61e05142c4c60740383e507409f1ec
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-23 16:33:56 +0100

    date

--- firebaseService.js
+++ firebaseService.js
@@ -116,7 +116,7 @@ async function updateAppointmentsType() {
     const appointmentsCollection = db.collection('appointments');
 
     // Query appointments for today
-    const querySnapshot = await appointmentsCollection.where('appointmentDate', '==', algeriaDate).get();
+    const querySnapshot = await appointmentsCollection.where('date', '==', algeriaDate).get();
 
     // Update "type" field to 0 for each document
     const updatePromises = querySnapshot.docs.map(async (doc) => {
