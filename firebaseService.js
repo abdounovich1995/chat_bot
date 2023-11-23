cf7f3fa301f4506658b11b019704007d98733f2c
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-23 15:16:28 +0100

    - hour

--- firebaseService.js
+++ firebaseService.js
@@ -97,7 +97,7 @@ async function addUserToClientCollection(userId) {
 }
 
 
-cron.schedule('15 15 * * *', async () => {
+cron.schedule('14 18 * * *', async () => {
   try {
     // Call a function to update "type" field in appointments collection to 0 for today's appointments
     await updateAppointmentsType();
