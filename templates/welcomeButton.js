0b737127ae9ef7f774528736a491b59d6f541703
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-28 16:48:56 +0100

    back

--- templates/welcomeButton.js
+++ templates/welcomeButton.js
@@ -1,30 +1,18 @@
-const axios = require('axios');
+
+const axios = require('axios'); // Import the axios library
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const SITE_URL = process.env.SITE_URL;
+const SITE_URL =process.env.SITE_URL;
 
-async function setWhitelistedDomains() {
-  const appSettings = {
-    whitelisted_domains: ["https://facebook-bot-demo-production.up.railway.app/close"],
-  };
 
-  const accessToken = PAGE_ACCESS_TOKEN;
 
-  try {
-    // Update Messenger Profile with whitelisted domains
-    await axios.post(`https://graph.facebook.com/v18.0/me/messenger_profile?access_token=${accessToken}`, appSettings);
+  async function sendButtonTemplate(userId,userRef) {
 
-    console.log('Whitelisted domains set successfully.');
-  } catch (error) {
-    console.error('Unable to set whitelisted domains:', error.response.data);
-  }
-}
+try {
 
-async function sendButtonTemplate(userId, userRef) {
-  try {
-    // Set whitelisted domains before sending the button template
-    await setWhitelistedDomains();
+  
 
+  
     const requestBody = {
       recipient: { id: userId },
       message: {
@@ -34,33 +22,58 @@ async function sendButtonTemplate(userId, userRef) {
             template_type: 'button',
             text: '🤖كيف يمكنني خدمتك ؟',
             buttons: [
-             
               {
-                type: 'web_url',
-                url: `https://facebook-bot-demo-production.up.railway.app/close`,
-                title: ' Close',
-                messenger_extensions: true,
-                webview_height_ratio: 'tall',
-                webview_share_button: 'hide',
-              },
-            ],
-          },
-        },
-      },
-    };
+                type: 'web_url', // Change the button type to 'web_url'
+                url: `${SITE_URL}/redirectPage?clientPSID=${userId}`,
+                title: ' حـجـز مـوعـد 📅',
+                messenger_extensions :true,
+                webview_height_ratio:'tall',
+                webview_share_button:"hide",
 
+            },
+            {
+              type: 'web_url', // Change the button type to 'web_url'
+              url: `${SITE_URL}/client-profile-show?clientRef=${userRef}`,
+              title: ' حسابي 👔',
+              messenger_extensions :true,
+              webview_height_ratio:'tall',
+              webview_share_button:"hide",
+
+
+          },
+
+          {
+            type: 'web_url', 
+            url: `https://facebook-bot-demo-production.up.railway.app/close?siteUrl=${SITE_URL}`,
+            title: ' Close',
+            messenger_extensions :true,
+            webview_height_ratio:'tall',
+            webview_share_button:"hide",
+
+
+        },
+
+            ]
+            
+          }
+        }
+      }
+    };
+  
+  
     try {
-      // Send the button template
       const response = await axios.post(`https://graph.facebook.com/v18.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`, requestBody);
       console.log('Button template sent:', response.data);
     } catch (error) {
       console.error('Error sending button template:', error.response.data);
     }
   } catch (error) {
-    console.error('Error setting whitelisted domains:', error);
+  
   }
-}
+  }
 
-module.exports = {
-  sendButtonTemplate,
-};
+
+  module.exports = {
+    sendButtonTemplate,
+  
+  };
