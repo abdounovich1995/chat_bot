766bdefcfb05ace8cdb255400838d54d1047eab0
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-29 10:24:30 +0100

    tryc

--- payloads.js
+++ payloads.js
@@ -2,6 +2,8 @@
 const firebaseService = require('./firebaseService'); // Import your Firebase service module here
 const messageManager = require('./messageManager'); // Import your message manager module here
 const setMenu = require('./setPersistMenu'); // Import your message manager module here
+const welcomeButton = require('./templates/welcomeButton'); // Import the messageManager module
+
 async function handlePostback(webhookEvent) {
   if (webhookEvent.postback) {
     if (webhookEvent.postback.payload === 'GET_STARTED_PAYLOAD') {
@@ -9,6 +11,7 @@ async function handlePostback(webhookEvent) {
 
       await firebaseService.addUserToClientCollection(senderPsid);
 const userRef= await  firebaseService.getClientReferenceByPSID(senderPsid);
+      welcomeButton.sendButtonTemplate(senderPsid,userRef);
       setMenu.setPersistentMenu(senderPsid,userRef);
 
     } else if (webhookEvent.postback.payload === "TAKE_APPOINTEMENT") {
