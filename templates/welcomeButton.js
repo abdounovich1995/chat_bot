72718e842b5f7835160810b92e9e6cbb773912dc
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-29 10:17:23 +0100

    try generic template

--- templates/welcomeButton.js
+++ templates/welcomeButton.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const SITE_URL = process.env.SITE_URL;
 
-async function sendButtonTemplate(psid, userRef) {
-  const buttonTemplate = {
+async function sendButtonTemplate (psid, userRef) {
+  const genericTemplate = {
     recipient: {
       id: psid,
     },
@@ -12,33 +12,37 @@ async function sendButtonTemplate(psid, userRef) {
       attachment: {
         type: 'template',
         payload: {
-          template_type: 'button',
-          text: 'Choose an option:',
-          buttons: [
+          template_type: 'generic',
+          elements: [
             {
-              type: 'web_url',
-              url: `${SITE_URL}/redirectPage?clientPSID=${psid}`,
-              title: 'حـجـز مـوعـد 📅',
-              messenger_extensions: true,
-              webview_height_ratio: 'tall',
-              webview_share_button: 'hide',
+              title: 'Option 1',
+              subtitle: 'Description for Option 1',
+              buttons: [
+                {
+                  type: 'web_url',
+                  url: `https://facebook-bot-demo-production.up.railway.app/close`,
+                  title: 'Option 1 Title',
+                  messenger_extensions: true,
+                  webview_height_ratio: 'tall',
+                  webview_share_button: 'hide',
+                },
+              ],
             },
             {
-              type: 'web_url',
-              url: `${SITE_URL}/client-profile-show?clientRef=${userRef}`,
-              title: 'حسابي 👔',
-              messenger_extensions: true,
-              webview_height_ratio: 'tall',
-              webview_share_button: 'hide',
+              title: 'Option 2',
+              subtitle: 'Description for Option 2',
+              buttons: [
+                {
+                  type: 'web_url',
+                  url: `${SITE_URL}/option2?clientPSID=${psid}`,
+                  title: 'Option 2 Title',
+                  messenger_extensions: true,
+                  webview_height_ratio: 'tall',
+                  webview_share_button: 'hide',
+                },
+              ],
             },
-            {
-              type: 'web_url',
-              url: `https://facebook-bot-demo-production.up.railway.app/close?siteUrl=${SITE_URL}`,
-              title: 'Close web',
-              messenger_extensions: true,
-              webview_height_ratio: 'tall',
-              webview_share_button: 'hide',
-            },
+            // Add more options as needed
           ],
         },
       },
@@ -48,14 +52,11 @@ async function sendButtonTemplate(psid, userRef) {
   const accessToken = PAGE_ACCESS_TOKEN;
 
   try {
-    await axios.post(`https://graph.facebook.com/v18.0/me/messages?access_token=${accessToken}`, buttonTemplate);
-    console.log('Button template sent successfully to PSID:', psid);
+    await axios.post(`https://graph.facebook.com/v18.0/me/messages?access_token=${accessToken}`, genericTemplate);
+    console.log('Generic template sent successfully to PSID:', psid);
   } catch (error) {
-    console.error('Unable to send button template:', error.message || error);
+    console.error('Unable to send generic template:', error.message || error);
   }
 }
 
-
-module.exports = {
-  sendButtonTemplate,
-};
+module.exports = { sendButtonTemplate  };
