b4076eaee0ccb7ff54455125fe4ec43ed3d60a16
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-11-29 09:17:01 +0100

    code from gpt

--- templates/welcomeButton.js
+++ templates/welcomeButton.js
@@ -3,80 +3,56 @@ const axios = require('axios');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const SITE_URL = process.env.SITE_URL;
 
-async function sendGenericTemplate(userId, userRef) {
-  try {
-    const requestBody = {
-      recipient: { id: userId },
-      message: {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'generic',
-            elements: [
-              {
-                title: 'حـجـز مـوعـد 📅',
-                subtitle: 'احجز موعد الآن',
-                image_url: `${SITE_URL}/booking_image.jpg`, // Replace with the URL of an image
-                default_action: {
-                  type: 'web_url',
-                  url: `${SITE_URL}/redirectPage?clientPSID=${userId}`,
-                  messenger_extensions: true,
-                  webview_height_ratio: 'tall',
-                  webview_share_button: false,
-                },
-                buttons: [
-                  {
-                    type: 'web_url',
-                    url: `${SITE_URL}/redirectPage?clientPSID=${userId}`,
-                    title: 'احجز الآن',
-                    messenger_extensions: true,
-                    webview_height_ratio: 'tall',
-                    webview_share_button: false,
-                  },
-                ],
-              },
-              {
-                title: 'حسابي 👔',
-                subtitle: 'عرض حساب المستخدم',
-                image_url: `${SITE_URL}/profile_image.jpg`, // Replace with the URL of an image
-                default_action: {
-                  type: 'web_url',
-                  url: `${SITE_URL}/client-profile-show?clientRef=${userRef}`,
-                  messenger_extensions: true,
-                  webview_height_ratio: 'tall',
-                  webview_share_button: false,
-                },
-                buttons: [
-                  {
-                    type: 'web_url',
-                    url: `${SITE_URL}/client-profile-show?clientRef=${userRef}`,
-                    title: 'عرض الحساب',
-                    messenger_extensions: true,
-                    webview_height_ratio: 'tall',
-                    webview_share_button: false,
-                  },
-                ],
-              },
-            ],
-          },
+async function sendButtonTemplate(psid, userRef) {
+  const buttonTemplate = {
+    recipient: {
+      id: psid,
+    },
+    message: {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: 'Choose an option:',
+          buttons: [
+            {
+              type: 'web_url',
+              url: `${SITE_URL}/redirectPage?clientPSID=${psid}`,
+              title: 'حـجـز مـوعـد 📅',
+              messenger_extensions: true,
+              webview_height_ratio: 'tall',
+              webview_share_button: 'hide',
+            },
+            {
+              type: 'web_url',
+              url: `${SITE_URL}/client-profile-show?clientRef=${userRef}`,
+              title: 'حسابي 👔',
+              messenger_extensions: true,
+              webview_height_ratio: 'tall',
+              webview_share_button: 'hide',
+            },
+            {
+              type: 'web_url',
+              url: `https://facebook-bot-demo-production.up.railway.app/close?siteUrl=${SITE_URL}`,
+              title: 'Close web',
+              messenger_extensions: true,
+              webview_height_ratio: 'tall',
+              webview_share_button: 'hide',
+            },
+          ],
         },
       },
-    };
+    },
+  };
 
-    try {
-      const response = await axios.post(
-        `https://graph.facebook.com/v18.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
-        requestBody
-      );
-      console.log('Generic template sent:', response.data);
-    } catch (error) {
-      console.error('Error sending generic template:', error.response.data);
-    }
+  const accessToken = PAGE_ACCESS_TOKEN;
+
+  try {
+    await axios.post(`https://graph.facebook.com/v18.0/me/messages?access_token=${accessToken}`, buttonTemplate);
+    console.log('Button template sent successfully to PSID:', psid);
   } catch (error) {
-    // Handle any other errors that might occur
+    console.error('Unable to send button template:', error.message || error);
   }
 }
 
-module.exports = {
-  sendGenericTemplate,
-};
+module.exports = { sendButtonTemplate };
